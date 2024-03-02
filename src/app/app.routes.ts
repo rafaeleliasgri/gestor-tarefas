ab3b340ee29718eee4be803e6ed349de9bb8f0a0
Rafael Elias Grigolo <rafaeleliasgri@alumni.usp.br>
2024-03-01 21:54:42 -0300

    criando os componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskDetailsComponent } from './task-details/task-details.component';
+import { TaskFilterComponent } from './task-filter/task-filter.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'tasklist', title: 'Lista de Tarefas', component: TaskListComponent},
+    {path: 'taskdetails', title: 'Detalhes da Tarefa', component: TaskDetailsComponent},
+    {path: '', redirectTo: '/tasklist', pathMatch: 'full'}
+
+];
